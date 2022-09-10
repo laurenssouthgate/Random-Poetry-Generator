ba261a08cc3d236aa8171455627afe6436b0b0c6
Laurens <laurenssouthgate@gmail.com>
2022-09-10 20:55:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-
-
-const sentences = [
-    "'A ' + randomAdjective() + ' ' + randomNoun() + ' ' + randomVerb() + 's a ' + randomAdjective() + ' ' + randomNoun()",
-    "'Please don`t ' + randomVerb() + ' my ' + randomAdjective() + ' ' + randomNoun()",
-    "'When ' + randomNoun() + 's appear they make my ' + randomAdjective() + ' ' + randomNoun() + ' ' + randomVerb()",
-    "'If only my ' + randomAdjective() + ' ' + randomNoun() + ' would ' + randomVerb() + ' my ' + randomAdjective() + ' ' + randomNoun()",
-    "'Your ' + randomNoun() + 's are like a ' + randomAdjective() + ' ' + randomNoun()",
-]
-
-
 $.ajax({
     type: 'GET',
     url: "https://raw.githubusercontent.com/laurenssouthgate/Random-Poetry-Generator/main/words.json",
@@ -26,13 +15,21 @@ $.ajax({
             const adjective = data[0].adjectives[Math.floor(Math.random() * data[0].adjectives.length)];
             return adjective;
         }
+        const sentences = [    
+            `A ${randomAdjective()} ${randomNoun()} ${randomVerb()}'s a ${randomAdjective()} ${randomNoun()}`,
+            `Please don't ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
+            `When ${randomNoun()}'s appear they make my ${randomAdjective()} ${randomNoun()} ${randomVerb()}`,
+            `If only my ${randomAdjective()} ${randomNoun()} would ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
+            `Your ${randomNoun()}'s are like a ${randomAdjective()} ${randomNoun()}`
+        ]
         $('.parameters-btn').on('click', function(e) {
             e.preventDefault();
-            const poemLength = $('.line-number').val();
+            const poemLength = $('.line-number').val()
             for (var i = 0; i < poemLength; i++) {
-                const randomSentence = eval(sentences[Math.floor(Math.random() * sentences.length)]);
-                $('.poem').append(randomSentence + '<br>');
+                const str = sentences[Math.floor(Math.random() * sentences.length)];
+                console.log(str)
+                $('.poem').append(str + '<br>');
             }
-        });
+        })
     }
 })
