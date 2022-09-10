cc4e14c5c6775263d3c6a6dbf45957aa2a0b0c58
Laurens <laurenssouthgate@gmail.com>
2022-09-10 22:00:56 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ $.ajax({
     url: "https://raw.githubusercontent.com/laurenssouthgate/Random-Poetry-Generator/main/words.json",
     dataType: 'json',
     success: function(data) {
+        const d = JSON.parse(data)
         const randomNoun = function(){
-            const noun = data[0].nouns[Math.floor(Math.random() * data[0].nouns.length)];
+            const noun = d[0].nouns[Math.floor(Math.random() * d[0].nouns.length)];
             return noun;
         }
         const randomVerb = function(){
-            const verb = data[0].verbs[Math.floor(Math.random() * data[0].verbs.length)];
+            const verb = d[0].verbs[Math.floor(Math.random() * d[0].verbs.length)];
             return verb;
         }
         const randomAdjective = function(){
-            const adjective = data[0].adjectives[Math.floor(Math.random() * data[0].adjectives.length)];
+            const adjective = d[0].adjectives[Math.floor(Math.random() * d[0].adjectives.length)];
             return adjective;
         }
         $('.parameters-btn').on('click', function(e) {
@@ -24,7 +25,8 @@ $.ajax({
                     `Please don't ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
                     `When ${randomNoun()}'s appear they make my ${randomAdjective()} ${randomNoun()} ${randomVerb()}`,
                     `If only my ${randomAdjective()} ${randomNoun()} would ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
-                    `Your ${randomNoun()}'s are like a ${randomAdjective()} ${randomNoun()}`
+                    `Your ${randomNoun()}'s are like a ${randomAdjective()} ${randomNoun()}`,
+                    `The sky looked like a ${randomAdjective()} ${randomNoun()}`
                 ]
                 const str = sentences[Math.floor(Math.random() * sentences.length)];
                 $('.poem').append(str + '<br>');
