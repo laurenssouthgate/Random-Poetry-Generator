cb8aeaf69e2f7813fbb68fda740bac6a75231f99
Laurens <laurenssouthgate@gmail.com>
2022-09-10 21:28:37 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -15,19 +15,18 @@ $.ajax({
             const adjective = data[0].adjectives[Math.floor(Math.random() * data[0].adjectives.length)];
             return adjective;
         }
-        const sentences = [    
-            `A ${randomAdjective()} ${randomNoun()} ${randomVerb()}'s a ${randomAdjective()} ${randomNoun()}`,
-            `Please don't ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
-            `When ${randomNoun()}'s appear they make my ${randomAdjective()} ${randomNoun()} ${randomVerb()}`,
-            `If only my ${randomAdjective()} ${randomNoun()} would ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
-            `Your ${randomNoun()}'s are like a ${randomAdjective()} ${randomNoun()}`
-        ]
         $('.parameters-btn').on('click', function(e) {
             e.preventDefault();
             const poemLength = $('.line-number').val()
             for (var i = 0; i < poemLength; i++) {
+                const sentences = [    
+                    `A ${randomAdjective()} ${randomNoun()} ${randomVerb()}'s a ${randomAdjective()} ${randomNoun()}`,
+                    `Please don't ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
+                    `When ${randomNoun()}'s appear they make my ${randomAdjective()} ${randomNoun()} ${randomVerb()}`,
+                    `If only my ${randomAdjective()} ${randomNoun()} would ${randomVerb()} my ${randomAdjective()} ${randomNoun()}`,
+                    `Your ${randomNoun()}'s are like a ${randomAdjective()} ${randomNoun()}`
+                ]
                 const str = sentences[Math.floor(Math.random() * sentences.length)];
-                console.log(str)
                 $('.poem').append(str + '<br>');
             }
         })
